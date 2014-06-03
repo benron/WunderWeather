c5473b48d0bc2f8d0953f70d39d90dea17c4ef95
Benjamin Pilcher <ben.pilcher@tackmobile.com>
2014-06-03 13:09:39 -0600

    Initial Commit

---
+++ WunderWeatherTests/WunderWeatherTests.m
@@ -0,0 +1,39 @@
+//
+//  WunderWeatherTests.m
+//  WunderWeatherTests
+//
+//  Created by Ben Pilcher on 6/3/14.
+//  Copyright (c) 2014 Pearson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WunderWeatherTests : XCTestCase
+
+@end
+
+@implementation WunderWeatherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
